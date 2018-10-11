85129dd9640f377c05d618edd94f9be5e7ac93d9
Rob Sullivan <robertosullivan5@gmail.com>
2018-10-11 11:36:33 +0100

    create notes component and added it to app. passed hard coded props data before connecting with firebase.

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,41 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import Note from "./Note/Note";
 import './App.css';
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+
+        //setup the react state of notes component
+        this.state = {
+            notes: [
+                {id: 1, noteContent: "Note 1 here!"},
+                {id: 2, noteContent: "Note 2 here!" },
+            ],
+        }
+    }
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      return (
+          <div className="notesWrapper">
+              <div className="notesHeader">
+                  <div className="heading">React & Firebase To-Do List</div>
+              </div>
+              <div className="notesBody">
+                  {
+                      this.state.notes.map((note) => {
+                          return (
+                              <Note noteContent={note.noteContent} noteId={note.id} key={note.id} />
+                          )
+                      })
+                  }
+              </div>
+              <div className="notesFooter">
+                  Footer will go here...save 19:03 --> https://www.youtube.com/watch?v=-RtJroTMDf4&t=2981s
+              </div>
+
+          </div>
     );
   }
 }
