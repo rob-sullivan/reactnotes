e666ba746015210b7c92649ffa62e2bb120244ba
Rob Sullivan <robertosullivan5@gmail.com>
2018-10-14 09:55:32 +0100

    connected firebase to app, added delete note functionality.

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,70 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import Note from "./Note/Note";
 import NoteForm from "./NoteForm/NoteForm";
+import { DB_CONFIG } from './Config/config';
+import firebase from 'firebase/app';
+import 'firebase/database';
 import './App.css';
 
 class App extends Component {
+
+    //this method gets called first.
     constructor(props) {
         super(props);
         this.addNote = this.addNote.bind(this);
+        this.removeNote = this.removeNote.bind(this);
+
+        this.app = firebase.initializeApp(DB_CONFIG);
+        this.database = this.app.database().ref().child('notes');
+
         //setup the react state of notes component
         this.state = {
-            notes: [
-                {id: 1, noteContent: "Note 1 here!"},
-                {id: 2, noteContent: "Note 2 here!" },
-            ],
+            notes: [],
         }
     }
 
-    addNote(note) {
-        
+    componentWillMount() {
         const previousNotes = this.state.notes;
-        previousNotes.push({ id: previousNotes.length + 1, noteContent: note});
-        this.setState({
-            notes: previousNotes
-        });
-        
+
+        //data from firestore is pulled into data snapshot object
+        //snap.val() is used to inspect that object
+        this.database.on('child_added', snap => {
+            previousNotes.push({
+                id: snap.key,
+                noteContent: snap.val().noteContent,
+            })
+
+            //update state with new array
+            this.setState({
+                notes: previousNotes
+            })
+        })
+
+        //when a child is removed from firestore find it in
+        // our array and remove it
+        this.database.on('child_removed', snap => {
+            for (var i = 0; i < previousNotes.length; i++) {
+                if (previousNotes[i].id === snap.key) {
+                    previousNotes.splice(i, 1);
+                }
+            }
+
+            //update state with new array
+            this.setState({
+                notes: previousNotes
+            })
+        })
+
     }
+
+    addNote(note) {
+        this.database.push().set({ noteContent: note });
+    }
+
+    removeNote(noteId) {
+        this.database.child(noteId).remove();
+    }
+
     render() {
         return (
             <div className="notesWrapper">
@@ -36,7 +75,12 @@ class App extends Component {
                     {
                         this.state.notes.map((note) => {
                             return (
-                                <Note noteContent={note.noteContent} noteId={note.id} key={note.id} />
+                                <Note
+                                    noteContent={note.noteContent}
+                                    noteId={note.id}
+                                    key={note.id}
+                                    removeNote={this.removeNote}
+                                />
                             )
                         })
                     }
--- client/src/Note/Note.jsx
+++ client/src/Note/Note.jsx
@@ -7,10 +7,16 @@ class Note extends Component {
         super(props);
         this.noteContent = props.noteContent;
         this.noteId = props.noteId;
+        this.handleRemoveNote = this.handleRemoveNote.bind(this);
+    }
+
+    handleRemoveNote(id) {
+        this.props.removeNote(id);
     }
     render(props) {
         return (
             <div className="note fade-in">
+                <span className="closebtn" onClick={() => this.handleRemoveNote(this.noteId)}>X</span>
                 <p className="noteContent">{ this.noteContent }</p>
             </div>
             )
