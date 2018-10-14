a25789957e8ef292a6697531828964770e160326
Rob Sullivan <5844677+rob-sullivan@users.noreply.github.com>
2018-10-14 16:04:07 +0700

    Delete config.js
    Deleted as it should not have been made public.

--- client/src/Config/config.js
+++
@@ -1,8 +0,0 @@
-export const DB_CONFIG = {
-    apiKey: "AIzaSyBEjQdpXI13ohofxSN7h0P3h_5ThN2YtOg",
-authDomain: "react-notes-2cbc7.firebaseapp.com",
-databaseURL: "https://react-notes-2cbc7.firebaseio.com",
-projectId: "react-notes-2cbc7",
-storageBucket: "react-notes-2cbc7.appspot.com",
-messagingSenderId: "480619297121"
-};
